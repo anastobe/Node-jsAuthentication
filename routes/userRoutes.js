9083b51fdb6e83c7f7ba62c72b0234ca646d7f24
anastobe <anastobe968@gmail.com>
2023-08-24 19:30:51 +0500

    anas home 24/8/23

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ import checkUserAuth from '../middlewares/auth-middleware.js';
 // ROute Level Middleware - To Protect Route
 router.use('/changepassword', checkUserAuth)
 router.use('/loggeduser', checkUserAuth)
+router.use('/vendors', checkUserAuth)
 
 // Public Routes
 router.post('/register', UserController.userRegistration)
@@ -16,6 +17,18 @@ router.post('/reset-password/:id/:token', UserController.userPasswordReset)
 // Protected Routes
 router.post('/changepassword', UserController.changeUserPassword)
 router.get('/loggeduser', UserController.loggedUser)
+// router.put('/updateProfile', UserController.upDateProfile)
+
+
+//user/client routes
+router.get('/vendors', UserController.allVendors)
+router.post('/user-add-job', UserController.userAddJob)
+router.get('/view-vendor-jobs', UserController.viewVendorsJobs)
+
+
+//user/provider routes
+router.get('/view-user-jobs', UserController.viewUserJobs)
+router.post('/vendor-add-job', UserController.vendorAddJob)
 
 
 export default router
